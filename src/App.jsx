7c291a2fbb57491323e18bffdd63485c7bf026e8
Rudra Patel <rudrapatel648673@gmail.com>
2026-05-17 23:45:42 +0530

    Move Checkput in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes , Route} from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
   return (
       <Routes>
         <Route index element={<HomePage/>} />
-        <Route path="/checkout" element={<>Checkout</>} />
+        <Route path="/checkout" element={<CheckoutPage/>} />
       </Routes>
-      
   )
 }
 
